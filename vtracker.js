55c30c56b0d6c620e28300dba050b03b4f9133d5
jpatel0 <jaypatel9670@gamil.com>
2020-01-18 21:58:56 +0530

    Added function onbeforeunload

--- vtracker.js
+++ vtracker.js
@@ -1,5 +1,6 @@
 var vid = document.getElementById("myVideo");
 var t = document.getElementById("r");
+
 // Calculate Played Time Function
 function calcPlayedTime(){
     var vid = document.getElementById("myVideo");
@@ -12,18 +13,42 @@ function calcPlayedTime(){
         t.innerHTML = t.innerHTML + vid.played.start(i) + "   " + vid.played.end(i) + "<br>";
         watchtime += vid.played.end(i) - vid.played.start(i);
         console.log(vid.played.start(i)+" "+vid.played.end(i));
-        // String.toString
     }
     console.log("total duration:"+vid.duration);
     console.log("total watch duration:"+watchtime);
 }
-vid.onended = calcPlayedTime;
+vid.onended = uploadData;
+
+window.onbeforeunload = function() {
+    this.console.log('Calling upload from on beforeload');
+    this.uploadData();
+    return "Dude, are you sure you want to leave? Think of the kittens!";
+}
+
+
+
+
+function uploadData(){
+    console.log('Uploading..');
+}
+
+
+
+
 vid.onratechange = () => {
     if(vid.playbackRate>2){
         console.log('speed limit -> 2')
         vid.playbackRate = 2;
     }
 };
+
+vid.addEventListener("DOMAttrModified", function(event) {
+    if (event.attrName == "src") {
+       // The `src` attribute changed!
+       calcPlayedTime();
+    }
+});
+
 vid.onseekend = () => {
     var vid = document.getElementById("myVideo");
     console.log(vid.currentTime)
@@ -32,8 +57,11 @@ vid.onseekend = () => {
 window.onhashchange = () => {
     alert('stop ');
 };
-function myFunction() { 
+function playNext() { 
   //alert("Start: " + vid.played.start(0) + " End: "  + vid.played.end(0));
-    var t = document.getElementById("r");
-    t.innerHTML = vid.played.start(1);
+  if(vid.getAttribute('src')=='gru.mp4'){
+    vid.setAttribute('src','media/nlp.mp4')
+  }else{
+    vid.setAttribute('src','media/gru.mp4')
+  }
 }
